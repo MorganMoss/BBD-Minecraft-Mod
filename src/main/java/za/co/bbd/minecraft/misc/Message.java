986c7648de463bf4f2e37c0aad953d3b3f09b685
Morgan Moss <morgan@bbd.co.za>
2023-03-16 23:16:30 +0200

    Data classes

---
+++ src/main/java/za/co/bbd/minecraft/misc/Message.java
@@ -0,0 +1,3 @@
+package za.co.bbd.minecraft.misc;
+
+public record Message(Role role, String content) { }
---
+++ src/main/java/za/co/bbd/minecraft/misc/Role.java
@@ -0,0 +1,7 @@
+package za.co.bbd.minecraft.misc;
+
+public enum Role {
+    SYSTEM,
+    USER,
+    ASSISTANT;
+}
