456fc3a921963979cbafaa79b35cbe8ce07ae0b0
DanielSBBD <daniels@bbd.co.za>
2023-03-20 23:35:17 +0200

    Added on and off states to receiver block

--- src/main/java/za/co/bbd/minecraft/blocks/RedstoneReceiverBlock.java
+++ src/main/java/za/co/bbd/minecraft/blocks/RedstoneReceiverBlock.java
@@ -3,22 +3,57 @@ package za.co.bbd.minecraft.blocks;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class RedstoneReceiverBlock extends Block {
-  public RedstoneReceiverBlock(AbstractBlock.Settings settings) {
-    super(settings);
-  }
 
-  @Override
-  public boolean emitsRedstonePower(BlockState state) {
-    return true;
-  }
+    public static final BooleanProperty ACTIVE = BooleanProperty.of("active");
 
-  @Override
-  public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
-    return 15;
-  }
+    public RedstoneReceiverBlock(AbstractBlock.Settings settings) {
+        super(settings);
+        setDefaultState(getDefaultState().with(ACTIVE, false));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(ACTIVE);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand,
+            BlockHitResult hit) {
+        if (world.getBlockState(pos).get(ACTIVE)) {
+            player.playSound(SoundEvents.BLOCK_IRON_TRAPDOOR_CLOSE, 1, 1);
+            world.setBlockState(pos, state.with(ACTIVE, false));
+        } else {
+            player.playSound(SoundEvents.BLOCK_IRON_TRAPDOOR_OPEN, 1, 1);
+            world.setBlockState(pos, state.with(ACTIVE, true));
+        }
+
+        return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public boolean emitsRedstonePower(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        if (world.getBlockState(pos).get(ACTIVE)) {
+            return 15;
+        } else {
+            return 0;
+        }
+    }
 }
